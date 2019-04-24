b59baf2839df9425c6027af54a69ba6b94a7d3d3
Wojciech Wetula <wetula.wojciech@gmail.com>
2019-04-24 15:08:44 +0200

    Bardziej "akademicki" tekst

--- python_app/app.py
+++ python_app/app.py
@@ -4,39 +4,36 @@ from pyspark import SparkContext
 
 
 text = """
-I met a boy wearing Vans, 501s
-And a dope Beastie tee, nipple rings, new tattoos
-That claimed that he was OGT, back from '92, from the first EP
-And in between sips of Coke
-He told me that he thought we were selling out
-Laying down, sucking up to the man
+Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit. 
+Aenean sit amet maximus neque, 
+vitae semper erat. Sed quis mi bibendum, 
+ultricies sem et, congue ligula. 
+Curabitur malesuada dolor quis nulla volutpat tincidunt. 
+Pellentesque et iaculis odio. 
+Vestibulum nec dolor et leo maximus blandit. 
+Mauris ultrices semper felis, 
+id ullamcorper mauris porttitor eget. 
+Etiam tristique orci nec dolor semper aliquet. 
+Nunc aliquam vestibulum turpis, 
+quis pharetra dui fermentum quis. 
+In nulla lectus, ornare sed ante in, 
+consequat rhoncus nisl. 
+Sed blandit erat eget felis cursus sollicitudin at at odio. 
+Orci varius natoque penatibus et magnis dis parturient montes, 
+nascetur ridiculus mus. Morbi leo neque, vestibulum a pharetra non, 
+fermentum eget elit. Donec sollicitudin justo augue, 
+vitae tincidunt enim viverra vitae. Nulla a libero venenatis, 
+consectetur nunc a, rhoncus quam.
 
-Well now I've got some advice for you, little buddy
-Before you point the finger you should know that I'm the man
-And if I'm the man, then you're the man, and he's the man as well
-So you can point that fucking finger up your ass
-
-All you know about me is what I've sold you, dumb fuck
-I sold out long before you ever heard my name
-I sold my soul to make a record, dipshit, and then you bought one
-
-So I've got some advice for you, little buddy
-Before you point your finger, you should know that I'm the man
-If I'm the fucking man then you're the fucking man as well
-So you can point that fucking finger up your ass
-
-All you know about me is what I've sold you, dumb fuck
-I sold out long before you ever heard my name
-I sold my soul to make a record, dipshit, and then you bought one
-
-All you read and wear or see and hear on TV Is a product Begging for your fatass dirty dollar
-Shut up and buy, buy, buy my new record
-And buy, buy, buy
-Send more money
-Fuck you, buddy
-Fuck you, buddy
-Fuck you, buddy
-Fuck you, buddy
+Nulla orci est, imperdiet eget congue in, 
+feugiat vitae quam. Duis sollicitudin tortor ac nulla porta dignissim. 
+In ac scelerisque sem. Nunc gravida ipsum nisi, 
+a ornare sapien aliquam eget. Cras gravida leo libero,
+ac aliquam ex pharetra nec. 
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
+Vivamus ut velit eget nisl maximus finibus sit amet at urna. 
+Aliquam erat volutpat. Pellentesque lorem magna, tempus vitae iaculis. 
 """
 
 
