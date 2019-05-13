b5bc9754cb79590ec5d6661467cfb5633bd58ed2
Isild <pimok96@gmail.com>
2019-05-13 22:24:04 +0200

    niedokończone czytanie JSONa z url, dane do zapisu do JSONa(dane bitowe do string)

--- python_app/app.py
+++ python_app/app.py
@@ -5,15 +5,18 @@ import sys
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+import json
+import urllib.request
+import base64
 from pyspark import SparkContext
 
 c = -0.73+0.19j
+maxit = 0
 
-# p --griddla ktoregopoliczymy fraktal
+# p --griddla ktorego policzymy fraktal
 def julia_calculate(p):
-    maxit = 500
+    global c, maxit
     #c = -0.73+0.19j #-0.10+0.65j
-    global c
     z = p
     divtime = maxit + np.zeros(z.shape, dtype=int)
 
@@ -27,9 +30,9 @@ def julia_calculate(p):
     return divtime
 
 
-# c --griddla ktoregopoliczymy fraktal
+# p --griddla ktorego policzymy fraktal
 def mandelbrot_calculate(p):
-    maxit = 100
+    global maxit
     z = p
     divtime = maxit + np.zeros(z.shape, dtype=int)
 
@@ -42,35 +45,68 @@ def mandelbrot_calculate(p):
 
     return divtime
 
+
+#TO DO: zapytać czy to tak ma być
+def readJSON():
+    with urllib.request.urlopen("jakis adres") as url:
+        data = json.loads(url.read().decode())
+
+    return data
+
+
+#TO DO: obraz do base64
+# name - nazwa fraktala, x,y- rozdzielczość, re,im - dla jakiej liczby zespolonej policzono fraktal, maxIt - maksymalna liczba iteracji, img - w formacie base64
+def dataToJSON(name, x, y, liczbaZesp, maxIt, img):
+    data = {
+        "name": name,
+        "x": x,
+        "y": y,
+        #"liczbaZespolona": liczbaZesp.__str__(),
+        "maxit": maxIt,
+        "image": img
+    }
+
+    jsonData = json.dumps(data)
+
+    return jsonData
+
 if __name__ == "__main__":
 
-    #zmienne, które będą wczytywane z jsona
-    fraktalOption = "julia"                     #"mandelbrot" / "julia"#
-    c = -0.10+0.65j                             #liczba zespolona
-    h = 3000                                    #rozdzielczosc y
-    w = 3000                                    #rozdzielczosc x
-    p1 = -1.5                                   #obszary generowania obrazu
-    p2 = -1.5
-    k1 = 1.5
-    k2 = 1.5
+
+    #otrzymany json
+    x = '{ "name": "julia", "maxIt":200, "re":-0.10, "im":0.65, "h":300, "w":300, "p1":-1.5, "p2":-1.5, "k1":1.5, "k2":1.5 }'
+    #x = readJSON()
+    print("json x:", x )
+    y = json.loads(x)
+
+    # zmienne, które będą wczytywane z jsona
+    liczbaZesp = 0+0j
+    liczbaZesp += y["re"]
+    liczbaZesp += y["im"]*1j
+    fractalOption = y["name"]                   #"mandelbrot" / "julia"#
+    c = liczbaZesp                              #liczba zespolona
+    maxit = y["maxIt"]
+    h = y["h"]                                  #rozdzielczosc y
+    w = y["w"]                                  #rozdzielczosc x
+    p1 = y["p1"]                                #obszary generowania obrazu
+    p2 = y["p2"]
+    k1 = y["k1"]
+    k2 = y["k2"]
     #dodatkowo ewentualnie jakieś kolory
     ##########
 
     context = SparkContext("local", "first app")
 
-
     y, x = np.ogrid[p1:k1:h * 1j, p2:k2:w * 1j]
     grid = x + y * 1j  # gridh x w punktow
-
-    #print(grid)
 
     t0 = time.time()
 
     grid_rdd = context.parallelize(grid, 2)  # stworzenie RDD z 2 partycjami
 
-    if fraktalOption == "julia":
+    if fractalOption == "julia":
         grid_rdd2 = grid_rdd.map(julia_calculate) # stworzenie kolejnego RDD na podstawie istniejącego RDD, dla każdego elementu z grid_rdd wykonywana jest funkcja julia_calculate
-    elif fraktalOption == "mandelbrot":
+    elif fractalOption == "mandelbrot":
         grid_rdd2 = grid_rdd.map(mandelbrot_calculate)
 
     fractal = grid_rdd2.collect()  # collect() --zbieramy wyniki do drivera
@@ -84,4 +120,15 @@ if __name__ == "__main__":
 
     plt.imshow(fractal)
     plt.show()
-    plt.savefig("dane.png")
+    plt.savefig("fraktal.png")
+
+    with open("fraktal.png", "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode('ascii')
+
+        print("obraz po enkodzie:", encoded_string)
+
+        jsonToSend = dataToJSON(fractalOption, w, h, liczbaZesp, maxit, encoded_string)
+
+        print("Json do wysłania:", jsonToSend)
+
+        #wysłanie JSONA
