010fe5a6d449ded051ac34f4b5b5181f9be8a0bd
Alex <alex.pilkevych@gmail.com>
2019-05-16 01:12:19 +0300

    Added fractal endpoint and container spawning

--- django/my_project/urls.py
+++ django/my_project/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 
 
 class SignUp(generic.CreateView):
@@ -13,10 +14,17 @@ class SignUp(generic.CreateView):
     template_name = 'signup.html'
 
 
+def fractal_request_handler(request):
+    import os
+    os.system('cd ../python_app && docker-compose build --build-arg args="{}" app && docker-compose up -d'.format(request.body))
+    return HttpResponse('Fractal requested')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', SignUp.as_view(), name='signup'),
+    path('fractal/', fractal_request_handler),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
