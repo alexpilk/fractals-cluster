21e74d12aee72d7fc7ff929cd3f7e011e4865e66
Alex <alex.pilkevych@gmail.com>
2019-05-16 09:22:29 +0300

    CORS

--- django/my_project/settings.py
+++ django/my_project/settings.py
@@ -30,7 +30,10 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
