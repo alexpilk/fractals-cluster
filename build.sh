ef4417932720c5cc551453c33ad1d8e7de1b8b29
Wojciech Wetula <wetula.wojciech@gmail.com>
2019-05-28 21:41:08 +0200

    Poprawiony skrypt app

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 docker build -t spark-base:spark-2.4.3_hadoop-2.7 base/
 docker build -t spark-master:spark-2.4.3_hadoop-2.7 master/
 docker build -t spark-worker:spark-2.4.3_hadoop-2.7 worker/
+docker build -t spark-submit:spark-2.4.3_hadoop-2.7 submit/
